c02391ba0192e73de9c27e1c553276413bec5787
Line'R <SeriniTY320@gmail.com>
2022-04-29 00:34:04 +0300

    Update

--- app/src/main/java/ru/liner/vr360client/activity/MainActivity.java
+++ app/src/main/java/ru/liner/vr360client/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package ru.liner.vr360client.activity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.hengyi.fastvideoplayer.library.FastVideoPlayer;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -17,20 +20,23 @@ import ru.liner.vr360client.tcp.TCPDevice;
 import ru.liner.vr360client.tcp.UDPMulticast;
 import ru.liner.vr360client.utils.Constant;
 import ru.liner.vr360client.utils.Networks;
+import ru.liner.vr360client.views.LImageButton;
 
 
 public class MainActivity extends CoreActivity implements ITCPCallback {
+    private FastVideoPlayer videoPlayer;
     private TextView logView;
     private TCPClient tcpClient;
     private UDPMulticast udpMulticast;
+    private LImageButton syncDevicesButton;
 
     private void setupSocketConnection() throws IOException {
-        udpMulticast = new UDPMulticast(Constant.IP_REQUEST, Constant.MULTICAST_PORT);
+        udpMulticast = new UDPMulticast(Constant.SERVER_IP_PUBLISHER_HOST, Constant.SERVER_MULTICAST_PORT);
         udpMulticast.setMulticastCallback(new IMulticastCallback() {
             @Override
             public void onReceived(String data) {
                 if (Networks.isValidHost(data) && tcpClient == null) {
-                    tcpClient = new TCPClient(data, Constant.TCP_CONNECTION_PORT);
+                    tcpClient = new TCPClient(data, Constant.SERVER_TCP_CONNECTION_PORT);
                     tcpClient.connect(MainActivity.this);
                 }
             }
@@ -43,6 +49,14 @@ public class MainActivity extends CoreActivity implements ITCPCallback {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logView = findViewById(R.id.logView);
+        videoPlayer = findViewById(R.id.videoPlayer);
+        syncDevicesButton = findViewById(R.id.syncDevicesButton);
+        videoPlayer.setLive(true);
+        videoPlayer.setScaleType(FastVideoPlayer.SCALETYPE_16_9);
+        videoPlayer.setTitle("");
+        videoPlayer.setHideControl(true);
+
+
         try {
             setupSocketConnection();
         } catch (IOException e) {
@@ -50,25 +64,45 @@ public class MainActivity extends CoreActivity implements ITCPCallback {
             Toast.makeText(this, "Cannot create stable connection! Try again later.", Toast.LENGTH_SHORT).show();
             finish();
         }
+        syncDevicesButton.setClickCallback(new LImageButton.Callback() {
+            @Override
+            public void onClick(LImageButton button) {
+                try {
+                    setupSocketConnection();
+                    if(tcpClient != null)
+                        tcpClient.connect(MainActivity.this);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(!udpMulticast.isRunning())
+        if (!udpMulticast.isRunning())
             udpMulticast.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(udpMulticast.isRunning())
+        if (udpMulticast.isRunning())
             udpMulticast.stop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (tcpClient.isConnected())
+            tcpClient.getDevice().send("disconnect");
     }
 
     @Override
     public void onConnected(TCPDevice device) {
         udpMulticast.stop();
+        appendLog("Connection with http:/" + device.getInetAddress().toString() + " successful!");
     }
 
     @Override
@@ -78,7 +112,7 @@ public class MainActivity extends CoreActivity implements ITCPCallback {
 
     @Override
     public void onConnectionFailed(InetAddress inetAddress) {
-        Toast.makeText(this, "Connection with http:/"+inetAddress.toString()+" failed!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Connection with http:/" + inetAddress.toString() + " failed!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -88,7 +122,43 @@ public class MainActivity extends CoreActivity implements ITCPCallback {
 
     @Override
     public void onReceived(InetAddress inetAddress, String data) {
-
+        switch (data) {
+            case "play":
+                appendLog(data + " http://" + Networks.getHost(inetAddress) + ":" + Constant.SERVER_STREAM_VIDEO_PORT);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (TextUtils.isEmpty(videoPlayer.getUrl()))
+                            videoPlayer.setUrl("http://" + Networks.getHost(inetAddress) + ":" + Constant.SERVER_STREAM_VIDEO_PORT);
+                        videoPlayer.play();
+                    }
+                });
+                break;
+            case "pause":
+                appendLog(data + " http://" + Networks.getHost(inetAddress) + ":" + Constant.SERVER_STREAM_VIDEO_PORT);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        videoPlayer.pause();
+                    }
+                });
+                break;
+            case "stop":
+                appendLog(data + " http://" + Networks.getHost(inetAddress) + ":" + Constant.SERVER_STREAM_VIDEO_PORT);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        videoPlayer.stop();
+                    }
+                });
+                break;
+            case "disconnect":
+                appendLog("Disconnected from: http:/" + inetAddress.toString() + " successful!");
+                tcpClient.getDevice().send("disconnect");
+                videoPlayer.stop();
+                videoPlayer.hide(true);
+                break;
+        }
     }
 
 
--- app/src/main/java/ru/liner/vr360client/utils/Constant.java
+++ app/src/main/java/ru/liner/vr360client/utils/Constant.java
@@ -10,12 +10,10 @@ import java.io.File;
  * @created : 22.04.2022, пятница
  **/
 public class Constant {
-    public static final File videoDirectory = new File(Environment.getExternalStorageDirectory(), "VR-Videos");
-    public static final int PACKET_SIZE = 100000;
-
-    public static final String IP_REQUEST = "232.5.6.8";
-    public static final int MULTICAST_PORT = 1234;
-    public static final int TCP_CONNECTION_PORT = 9092;
-    public static final int TCP_FILES_PORT = 9091;
+    public static final int CHOOSE_VIDEO_REQUEST_CORE = 2283;
+    public static final int SERVER_STREAM_VIDEO_PORT = 8888;
+    public static final int SERVER_TCP_CONNECTION_PORT = 9092;
+    public static final int SERVER_MULTICAST_PORT = 1234;
+    public static final String SERVER_IP_PUBLISHER_HOST = "232.5.6.8";
 
 }
